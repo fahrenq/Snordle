69d471d7559a5bb25db1c23c22c008cf66ffcfc5
fahrenq <ivankuznetsov316@gmail.com>
2022-06-19 12:33:45 +0200

    controls improvements

--- src/App.fs
+++ src/App.fs
@@ -3,7 +3,6 @@ module App
 open Game
 open Browser.Dom
 open Queue
-open System.Collections.Generic
 
 let mutable field = emptyField 15 15
 field <- spawnSnake field 5
@@ -25,69 +24,35 @@ let render () =
 
 render ()
 
-let controls = ResizeArray<Direction>()
-
-// document.onkeydown <-
-//   fun e ->
-//     match e.code with
-//     | "ArrowUp" -> inputBuffer.TryEnqueue Up |> ignore
-//     | "ArrowRight" -> inputBuffer.TryEnqueue Right |> ignore
-//     | "ArrowDown" -> inputBuffer.TryEnqueue Down |> ignore
-//     | "ArrowLeft" -> inputBuffer.TryEnqueue Left |> ignore
-//     | _ -> ()
+let inputBuffer = new Queue<Direction>(3)
 
 document.onkeydown <-
   fun e ->
     match e.code with
-    | "ArrowUp" -> controls.Add Up
-    | "ArrowRight" -> controls.Add Right
-    | "ArrowDown" -> controls.Add Down
-    | "ArrowLeft" -> controls.Add Left
+    | "ArrowUp" -> inputBuffer.TryEnqueue Up |> ignore
+    | "ArrowRight" -> inputBuffer.TryEnqueue Right |> ignore
+    | "ArrowDown" -> inputBuffer.TryEnqueue Down |> ignore
+    | "ArrowLeft" -> inputBuffer.TryEnqueue Left |> ignore
     | _ -> ()
 
-document.onkeyup <-
-  fun e ->
-    let r = controls.Remove Down
-    writeLog (sprintf "KeyUp: %s, remove: %A" e.code r)
-    match e.code with
-    | "ArrowUp" -> controls.Remove Up |> ignore
-    | "ArrowRight" -> controls.Remove Right |> ignore
-    | "ArrowDown" -> controls.Remove Down |> ignore
-    | "ArrowLeft" -> controls.Remove Left |> ignore
-    | _ -> ()
 
-let inputBuffer = new Queue<Direction>(3)
-
-// let rec findNextDirection currentDirection (queue: Queue<Direction>) =
-//   match queue.TryDequeue() with
-//   | None -> currentDirection
-//   | Some newDirection when currentDirection = newDirection ->
-//     findNextDirection currentDirection queue
-//   | Some newDirection ->
-//     let _, _, canMove = moveSnake field 5 newDirection
-
-//     if canMove then
-//       newDirection
-//     else
-//       findNextDirection currentDirection queue
-
-let findNextDirection currentDirection (controls: ResizeArray<Direction>) =
-  if controls.Count = 0 then
-    currentDirection
-  else
-    let newDirection = controls.[0]
+let rec findNextDirection currentDirection (queue: Queue<Direction>) =
+  match queue.TryDequeue() with
+  | None -> currentDirection
+  // | Some newDirection when currentDirection = newDirection ->
+  //   findNextDirection currentDirection queue
+  | Some newDirection ->
     let _, _, canMove = moveSnake field 5 newDirection
-    newDirection
 
-    // if canMove then
-    // else
-    //   findNextDirection currentDirection queue
+    if canMove then
+      newDirection
+    else
+      findNextDirection currentDirection queue
 
 let rec loop (prevDirection: Direction) =
   async {
     do! Async.Sleep 300
-    writeLog <| controls.ToString()
-    let nextDirection = findNextDirection prevDirection controls
+    let nextDirection = findNextDirection prevDirection inputBuffer
 
     writeLog ("Next direction: " + nextDirection.ToString())
 
--- src/Script.fsx
+++ src/Script.fsx
@@ -3,19 +3,3 @@
 
 open Game
 
-// let directionsBuffer: Direction [] = Array.zeroCreate 3
-// printfn "%A" directionsBuffer
-
-// directionsBuffer[0] = Direction.Up
-
-// directionsBuffer |> Array.tryFindIndex isNull
-
-let a = ResizeArray<Direction>()
-a.Add Up
-a.Add Down
-
-a.RemoveAt 0
-
-a[0]
-
-a
